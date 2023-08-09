d6ffd3c523757fcb7ca6d42822a6fc1a7b3e7989
VremyaNeZhdet <3331315@mail.ru>
2023-08-09 21:58:31 +0300

    Add Menu

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
             <li><a href="">Menu Item 1</a></li>
             <li><a href="">Menu Item 2</a></li>
             <li><a href="">Menu Item 3</a></li>
+              <li><a href="">Menu Item 4</a></li>
           </ul>
         </Menu>
         <Box>
